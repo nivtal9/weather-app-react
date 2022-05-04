aee68ae78027050783e648f1dc36864ef7a2fc75
nivtal9 <nivtal9@gmail.com>
2022-05-04 20:27:34 +0300

    added elements
    adding country code, current time, flag and favorits button.
    also not yet working but logic implemnted - background change according to current time.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,46 @@ import axios from 'axios'
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
+  const [flagURL, setFlagURL] = useState('');
+  const [currTime, setCurrTime] = useState('');
+  var [background, setBackground] = useState('');
+
 
   const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=0eb35f3c632280096f5115f275d4b469`;
+  const flagAPI=`https://countryflagsapi.com/svg/`;
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(weatherAPI).then((response) => {
         setData(response.data);
+        setFlagURL(flagAPI + response.data.sys.country);
+
+        var currTime=Math.round(new Date().getTime()/1000)
+        currTime+=response.data.timezone;
+        var date = new Date(currTime*1000);
+        var timeString = date.toISOString();
+        setCurrTime(timeString.substring(11,16));
+        var hours=timeString.substring(11,13);
+        console.log(hours);
+        if(5<hours && hours<12){
+          console.log("morning");
+          setBackground("url(./assets/morning.jpg)");
+        }
+        else if(11<hours && hours<18){
+          console.log("mid-day");
+          setBackground("url(./assets/mid-day.jpg)");
+        }
+        else if((17<hours && hours<23) || (-1<hours && hours<6)){
+          console.log("night");
+          setBackground("url(./assets/night.jpg)");
+        }
       })
       setLocation('');
     }
   }
 
   return (
-    <div className="app">
+    <div className="app" style={{backgroundImage: `url(${background})`}}>
       <div className="search">
         <input
           value={location}
@@ -25,11 +51,13 @@ function App() {
           onKeyPress={searchLocation}
           placeholder='Enter Location'
           type="text" />
+          <button>favorits</button>
       </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>{data.name}</p>
+            <p>{data.name? [data.name,", ", data.sys.country, ", ", currTime] : null}</p>
+            {flagURL ? <img className="flag" src={flagURL} alt="flag" style={{width: 100, height: 50}}></img> : null}
           </div>
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}&#8451;</h1> : null}
@@ -50,7 +78,7 @@ function App() {
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} KM/h</p> : null}
               <p>Wind Speed</p>
             </div>
           </div>
